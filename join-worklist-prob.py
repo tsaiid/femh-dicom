08aee9260f4170045ad88491512bc0c01fffd3bc
I-Ta Tsai <ittsai@gmail.com>
2018-05-19 02:17:45 +0800

    add more detail sql

--- join-worklist-prob.py
+++ join-worklist-prob.py
@@ -1,31 +1,77 @@
 from sqlalchemy import create_engine
 from cxrdbcls import CxrNormalProbability
 import pandas as pd
+from os.path import join
+import yaml
 
 def main():
-    # get worklist
-    engine_wl = create_engine('oracle:///')
-    Session = sessionmaker(bind=engine_wl)
-    session = Session()
-    results = engine2.execute('select acc_no, exam_time from worklist limit 1000;')
-    wl_dict_list = [dict(row) for row in results]
-    session.close()
+    # load cfg
+    yml_path = join('config', 'cxr.yml')
+    with open(yml_path, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
 
     # get prob db
-    db_path = 'test_1.db'
-    engine_prob = create_engine('sqlite:///' + db_path)
-    Session = sessionmaker(bind=engine_prob)
-    session = Session()
-    results = engine_prob.execute('select acc_no, model_name, model_ver, normal_probability from cxr_normal_probability;')
+    oracle_conn_str = 'oracle+cx_oracle://{username}:{password}@{dsn_str}'
+    dsn_str = cx_Oracle.makedsn(cfg['oracle']['ip'], cfg['oracle']['port'], cfg['oracle']['service_name']).replace('SID', 'SERVICE_NAME')
+    engine_prob = create_engine(
+        oracle_conn_str.format(
+            username=cfg['oracle']['username'],
+            password=cfg['oracle']['password'],
+            dsn_str=dsn_str
+        )
+    )
+    #Session = sessionmaker(bind=engine_prob)
+    #session_prob = Session()
+    results = engine_prob.execute('select * from ml_predictions')
     prob_dict_list = [dict(row) for row in results]
-    session.close()
+    prob_df = pd.DataFrame(prob_dict_list)
+    #session.close()
+
+    # get worklist
+    dsn_str = cx_Oracle.makedsn(cfg['oracle2']['ip'], cfg['oracle2']['port'], cfg['oracle2']['service_name']).replace('SID', 'SERVICE_NAME')
+    engine_wl = create_engine(
+        oracle_conn_str.format(
+            username=cfg['oracle2']['username'],
+            password=cfg['oracle2']['password'],
+            dsn_str=dsn_str
+        )
+    )
+    #Session = sessionmaker(bind=engine)
+    #session = Session()
+    sql_get_worklist = '''
+SELECT  w.accno,
+        TO_CHAR(w.datadate, 'yyyy-mm-dd hh24:mi:ss') datadate_str,
+        w.datastatus,
+        TO_CHAR(w.examdate, 'yyyy-mm-dd hh24:mi:ss') examdate_str,
+        w.examcode,
+        w.examname
+FROM
+    (   SELECT id, accno, datadate, datastatus, examdate, examcode, examname, reportid
+        FROM risworklistdatas
+        WHERE
+            examcode IN ('RA014', 'RA015', 'RA016', 'RA017', 'RA018', 'RA021')
+            AND
+            examdate BETWEEN
+                TO_DATE( TO_CHAR( SYSDATE-30, 'yyyy-mm-dd' ), 'yyyy-mm-dd' )
+                    AND
+                TO_DATE( TO_CHAR( SYSDATE, 'yyyy-mm-dd' ) || ' 23:59:59', 'yyyy-mm-dd hh24:mi:ss' )
+        ORDER BY examdate DESC
+    ) w
+WHERE
+    w.datastatus <> 'RPTVS'
+    AND
+    ROWNUM < 2000
+    '''
+    results = engine_wl.execute(sql_get_worklist)
+    wl_dict_list = [dict(row) for row in results]
+    wl_df = pd.DataFrame(wl_dict_list)
+    #session.close()
 
     # use pandas df
-    wl_df = pd.DataFrame(wl_dict_list)
-    prob_df = pd.DataFrame(prob_dict_list)
-    df = wl_df.join(prob_df.set_index('acc_no'), on='acc_no', lsuffix='_left', rsuffix='_right')
-    sorted_df = df.loc[df['model_name'] == 'test_model1'] \
-                  .sort_values(by='normal_probability', ascending=False)
+    df = wl_df.join(prob_df.set_index('accno'), on='accno', lsuffix='_left', rsuffix='_right')
+    sorted_df = df.loc[(df['model_name'] == 'densenet121-binary-classification') & \
+                       (df['weights_name'] == 'femh-224-32')] \
+                  .sort_values(by='probability', ascending=False)
     print(sorted_df)
 
 if __name__ == "__main__":
