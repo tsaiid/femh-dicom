4630d1de62bcda5f50b0eed872d013d24576a754
I-Ta Tsai <ittsai@gmail.com>
2018-05-31 15:43:30 +0800

    change table name; clean code.

--- mldbcls.py
+++ mldbcls.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from sqlalchemy import Column, Integer, Float, String, DateTime
-from sqlalchemy.sql import func
+from sqlalchemy.sql import func, select, text
 from sqlalchemy.schema import Sequence
 
 @as_declarative()
@@ -10,15 +10,15 @@ class Base(object):
         return cls.__name__.lower()
 
 class MLPrediction(Base):
-    __tablename__ = 'ML_PREDICTIONS'
+    __tablename__ = 'RISML_PREDICTIONS'
 
-    PRED_ID = Column(Integer, Sequence('SEQ_ML_PREDICTIONS_PRED_ID'), primary_key=True, autoincrement=True)
-    ACCNO = Column(String(50),nullable=False)
-    MODEL_NAME = Column(String(50),nullable=False)
-    MODEL_VER = Column(String(50),nullable=False)
-    WEIGHTS_NAME = Column(String(50),nullable=False)
-    WEIGHTS_VER = Column(String(50),nullable=False)
-    CATEGORY = Column(String(50),nullable=False)
-    PROBABILITY = Column(Float(),nullable=False)
+    RED_ID = Column(String(100), primary_key=True, nullable=False)
+    ACCNO = Column(String(50), nullable=False)
+    MODEL_NAME = Column(String(50), nullable=False)
+    MODEL_VER = Column(String(50), nullable=False)
+    WEIGHTS_NAME = Column(String(50), nullable=False)
+    WEIGHTS_VER = Column(String(50), nullable=False)
+    CATEGORY = Column(String(50), nullable=False)
+    PROBABILITY = Column(Float(), nullable=False)
 #    time_created = Column(DateTime(timezone=True), server_default=func.now())
 #    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
