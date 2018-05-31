5be18c51934f1c050b8dc66d1089b7d78404eb4d
I-Ta Tsai <ittsai@gmail.com>
2018-05-31 23:23:02 +0800

    add a param to control use db or not

--- predict.py
+++ predict.py
@@ -23,6 +23,7 @@ from mldbcls import MLPrediction
 import time
 
 session = None
+_use_db = None
 
 def read_dcm_to_image(ds_or_file):
     if isinstance(ds_or_file, FileDataset):
@@ -54,18 +55,22 @@ def predict_image(img, model):
 def do_predict(models, path):
     results = []
     ds, img = read_dcm_to_image(path)
+    acc_no = ds.get('AccessionNumber', None)
+
     for model in models:
         # check if exists
-        acc_no = ds.get('AccessionNumber', None)
         model_name = model.model_name
         model_ver = model.model_ver
         weight_name = model.weight_name
         weight_ver = model.weight_ver
         category = model.category
-        is_exist = check_if_pred_exists(acc_no, model_name, model_ver, weight_name, weight_ver, category)
-        if is_exist:
-            print("{} {} {} exists, skip.".format(acc_no, model_name, weight_name))
-            continue
+
+        global _use_db
+        if _use_db:
+            is_exist = check_if_pred_exists(acc_no, model_name, model_ver, weight_name, weight_ver, category)
+            if is_exist:
+                print("{} {} {} exists, skip.".format(acc_no, model_name, weight_name))
+                continue
 
         small_img = img.resize((model.width, model.height))
         small_img = small_img.convert('L')
@@ -74,7 +79,7 @@ def do_predict(models, path):
         if ds.PhotometricInterpretation == 'MONOCHROME1':
             small_img = invert(small_img)
 
-        exam_time = ds.get('AcquisitionDate', None)
+        #exam_time = ds.get('AcquisitionDate', None)
         small_img_arr = np.array(small_img.convert('RGB'))
         prob = predict_image(small_img_arr, model.obj)
         result = { 'acc_no': acc_no,
@@ -100,9 +105,12 @@ def check_if_pred_exists(acc_no, model_name, model_ver, weight_name, weight_ver,
     return exists
 
 def main():
-    if len(sys.argv) is not 2:
-        print("argv")
+    if len(sys.argv) is not 3:
+        print("argv: target_path use_db")
         sys.exit(1)
+
+    global _use_db
+    _use_db = sys.argv[2] != '0'
 
     t_start = time.time()
 
@@ -111,20 +119,21 @@ def main():
     with open(yml_path, 'r') as ymlfile:
         cfg = yaml.load(ymlfile)
 
-    # init db engine
-    global session
-    #db_path = cfg['sqlite']['path']
-    oracle_conn_str = 'oracle+cx_oracle://{username}:{password}@{dsn_str}'
-    dsn_str = cx_Oracle.makedsn(cfg['oracle']['ip'], cfg['oracle']['port'], cfg['oracle']['service_name']).replace('SID', 'SERVICE_NAME')
-    engine = create_engine(
-        oracle_conn_str.format(
-            username=cfg['oracle']['username'],
-            password=cfg['oracle']['password'],
-            dsn_str=dsn_str
+    if _use_db:
+        # init db engine
+        global session
+        #db_path = cfg['sqlite']['path']
+        oracle_conn_str = 'oracle+cx_oracle://{username}:{password}@{dsn_str}'
+        dsn_str = cx_Oracle.makedsn(cfg['oracle']['ip'], cfg['oracle']['port'], cfg['oracle']['service_name']).replace('SID', 'SERVICE_NAME')
+        engine = create_engine(
+            oracle_conn_str.format(
+                username=cfg['oracle']['username'],
+                password=cfg['oracle']['password'],
+                dsn_str=dsn_str
+            )
         )
-    )
-    Session = sessionmaker(bind=engine)
-    session = Session()
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
     # load all models
     cxr_models = [CxrModel(m, w) for m in cfg['model'] for w in m['weight']]
@@ -167,19 +176,22 @@ def main():
                 return super(MyEncoder, self).default(obj)
 
     for r in results:
-        hash_pred_id = hashlib.sha256(json.dumps(r, sort_keys=True, cls=MyEncoder).encode('utf-8')).hexdigest()
-        session.add(MLPrediction(   RED_ID=hash_pred_id,
-                                    ACCNO=r['acc_no'],
-                                    MODEL_NAME=r['model_name'],
-                                    MODEL_VER=r['model_ver'],
-                                    WEIGHTS_NAME=r['weight_name'],
-                                    WEIGHTS_VER=r['weight_ver'],
-                                    CATEGORY=r['category'],
-                                    PROBABILITY=r['probability'] ))
-        session.commit()
-        #print(r['acc_no'], r['probability'], hash_pred_id)
+        if _use_db:
+            hash_pred_id = hashlib.sha256(json.dumps(r, sort_keys=True, cls=MyEncoder).encode('utf-8')).hexdigest()
+            session.add(MLPrediction(   RED_ID=hash_pred_id,
+                                        ACCNO=r['acc_no'],
+                                        MODEL_NAME=r['model_name'],
+                                        MODEL_VER=r['model_ver'],
+                                        WEIGHTS_NAME=r['weight_name'],
+                                        WEIGHTS_VER=r['weight_ver'],
+                                        CATEGORY=r['category'],
+                                        PROBABILITY=r['probability'] ))
+            session.commit()
+        else:
+            print(r)
 
-    session.close()
+    if _use_db:
+        session.close()
 
 if __name__ == "__main__":
     main()
