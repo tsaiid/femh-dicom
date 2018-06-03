253adf6b74b44195eb453ec2a91cb4389b27c0c9
I-Ta Tsai <ittsai@gmail.com>
2018-06-03 23:24:04 +0800

    mkdir before copy

--- copy-images-in-csv.py
+++ copy-images-in-csv.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import errno
 import shutil
 import pandas as pd
 
@@ -11,10 +12,22 @@ def main():
     csv_path = sys.argv[1]
     source_img_path = sys.argv[2]
     output_img_path = sys.argv[3]
+    had_checked_dir_exists = False
 
     df = pd.read_csv(csv_path)
     for index, row in df.iterrows():
-        src = os.path.join(source_img_path, row['ACCNO'] + '.png')
+        accno = row['ACCNO'].split('.png')[0]
+        src = os.path.join(source_img_path, accno + '.png')
+
+        if not had_checked_dir_exists and not os.path.exists(output_img_path):
+            try:
+                os.makedirs(output_img_path)
+            except OSError as exc: # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+            else:
+                had_checked_dir_exists = True
+
         shutil.copy2(src, output_img_path)
         #print(src, output_img_path)
         #if index > 3:
