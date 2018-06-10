e027aa67aad1a1fcf4592436740dff4a5ded496a
I-Ta Tsai <ittsai@gmail.com>
2018-06-11 01:23:28 +0800

    return pivot data

--- webapp/main.py
+++ webapp/main.py
@@ -1,12 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from config import DevConfig
-
-import cx_Oracle
-import yaml
-import os
-from sqlalchemy import create_engine
-from flask import jsonify
+from sqlalchemy import text
 
 #from mldbcls import MLPrediction
 
@@ -16,6 +11,7 @@ app = Flask(__name__)
 app.config.from_object(DevConfig)
 db = SQLAlchemy(app)
 
+"""
 class MLPrediction(db.Model):
     __tablename__ = 'ML_PREDICTIONS'
 
@@ -30,6 +26,7 @@ class MLPrediction(db.Model):
 
     def __repr__(self):
         return '<MLPrediction %r>' % self.probability
+"""
 
 # 路由和處理函式配對
 @app.route('/')
@@ -38,46 +35,44 @@ def index():
 
 @app.route('/prob/<accno>')
 def get_probability(accno):
-    """
-    yml_path = os.path.join('E:\\git\\femh-dicom\\config', 'cxr.yml')
-    with open(yml_path, 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
-
-    oracle_conn_str = 'oracle+cx_oracle://{username}:{password}@{dsn_str}'
-    dsn_str = cx_Oracle.makedsn(cfg['oracle']['ip'],
-                                cfg['oracle']['port'],
-                                cfg['oracle']['service_name']).replace('SID', 'SERVICE_NAME')
-    engine = create_engine(
-        oracle_conn_str.format(
-            username=cfg['oracle']['username'],
-            password=cfg['oracle']['password'],
-            dsn_str=dsn_str
-        )
+    sql = text('''
+    SELECT *
+    FROM (
+        SELECT accno, category, probability
+        FROM risml_predictions
+        WHERE accno='{accno}'
+        AND weights_name IN ('femh-224-32',
+                             'femh-224-32-ett',
+                             'femh-224-32-port-a',
+                             'femh-224-32-cardiomegaly')
     )
-
-    sql_get_worklist = '''
-    SELECT  *
-    FROM
-        (   SELECT *
-            FROM ml_predictions
-            WHERE
-                accno = '{accno}'
-            ORDER BY model_name, weights_name, probability DESC
-        )
-    WHERE
-        ROWNUM <= 10
-    '''.format(accno=accno)
-    results = engine.execute(sql_get_worklist)
-    """
-
-    pred = MLPrediction.query.filter_by(accno=accno).first()
-    """
-    prob_list = [{'accno': row['accno'],
-                'model_name': row['model_name'],
-                'weights_name': row['weights_name'],
-                'probability': str(row['probability'])} for row in results]
-    """
-    return str(pred.probability)
+    PIVOT (
+        MAX(probability)
+        FOR category IN ('cxr-normal' normal,
+                         'cxr-ett' ett,
+                         'cxr-port-a' port,
+                         'cxr-cardiomegaly' cardiomegaly)
+    )
+    '''.format(accno=accno))
+    results = db.engine.execute(sql)
+    row = results.fetchone()
+    if row:
+        result_dict = {
+            'success': 1,
+            'accno': row['accno'],
+            'probabilities': {
+                'normal': str(row['normal']),
+                'ett': str(row['ett']),
+                'port': str(row['port']),
+                'cardiomegaly': str(row['cardiomegaly'])
+            }
+        }
+    else:
+        result_dict = {
+            'success': 0,
+            'reason': 'No result for accno: {accno}'.format(accno=accno)
+        }
+    return jsonify(result_dict)
 
 # 判斷自己執行非被當做引入的模組，因為 __name__ 這變數若被當做模組引入使用就不會是 __main__
 if __name__ == '__main__':
