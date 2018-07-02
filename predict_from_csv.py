a205ba85cc0224f599be1965679c7153185fc4c0
I-Ta Tsai <ittsai@gmail.com>
2018-07-03 00:33:29 +0800

    add util to predict from csv

---
+++ predict_from_csv.py
@@ -0,0 +1,60 @@
+import os
+import numpy as np
+import pandas as pd
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.densenet import preprocess_input
+from PIL import Image
+import sys
+from tqdm import tqdm
+
+_model = None
+
+def global_keras_init():
+    global _model
+
+    model_path = 'models/keras/cxr-binary-classifier.h5'
+    weights_path = 'models/keras/weights/fake-224-densenet121-32-nipple3k.h5'
+    _model = load_model(model_path)
+    _model.load_weights(weights_path)
+
+def do_predict(png_path):
+    global _model
+
+    img = image.load_img(png_path, target_size = (224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = _model.predict(x)
+
+    return preds 
+
+def main():
+    if len(sys.argv) is not 4:
+        print("argv: csv_path png_dirpath csv_out_path")
+        sys.exit(1)
+
+    csv_path = sys.argv[1]
+    png_dirpath = sys.argv[2]
+    csv_out_path = sys.argv[3]
+
+    # init Keras model
+    global_keras_init()
+
+    df = pd.read_csv(csv_path)
+
+    def _calc_prob(row):
+        png_path = os.path.join(png_dirpath, row['ACCNO'] + '.png')
+        res = do_predict(png_path)
+        
+        return res[0][0] 
+
+    tqdm.pandas()
+
+    df['prob'] = df.progress_apply(_calc_prob, axis=1)
+    df = df.sort_values(by='prob', ascending=False)
+    df.to_csv(csv_out_path)
+    #print(df)
+
+if __name__ == "__main__":
+    main()
