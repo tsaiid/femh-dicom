7cacad0d0e657b5d95253d89994c708e987f1cec
I-Ta Tsai <ittsai@gmail.com>
2018-07-22 11:21:02 +0800

    clean code

--- retrieve_study.py
+++ retrieve_study.py
@@ -34,27 +34,6 @@ def retrieve_study(cfg, acc_no, output_dir):
                     pass
                 else:
                     print('status: {}; dataset: {}'.format(status_cm, dataset_cm))
-
-            """
-            p_id = dataset.PatientID
-            study_uid = dataset.StudyInstanceUID
-            series_uid = dataset.SeriesInstanceUID
-
-            cmd_str = r'''movescu {pacs_ip} {pacs_port} +P {port} +xa -aec {pacs_aet} -aet {aet} \
-                          -k QueryRetrieveLevel=SERIES \
-                          -k PatientID={p_id} \
-                          -k StudyInstanceUID={study_uid} \
-                          -k SeriesInstanceUID={series_uid} \
-                          -od {output_dir} \
-                        '''.format( pacs_ip=cfg['pacs']['called']['ip'], pacs_port=cfg['pacs']['called']['port'], pacs_aet=cfg['pacs']['called']['aet'],
-                                    aet=cfg['pacs']['my']['aet'], port=cfg['pacs']['my']['port'],
-                                    p_id=p_id, study_uid=study_uid, series_uid=series_uid,
-                                    output_dir=output_dir )
-            #print(cmd_str)
-            os.system(cmd_str)
-            #print("Success. acc_no={}".format(acc_no))
-            """
-
         elif status_cf.Status != 0x0:
             print("acc_no={}, status={}".format(acc_no, hex(status_cf.Status)))
     assoc_cf.release()
