cf42dc76e6a6dea44653adbe868b2f9bc8ee297d
I-Ta Tsai <ittsai@gmail.com>
2018-10-27 12:09:12 +0800

    change workflow

--- workflow/hourly-classification.sh
+++ workflow/hourly-classification.sh
@@ -3,13 +3,15 @@
 # Config
 WORKDIR=/app
 DCM_TMPDIR=/dcm_tmpdir
+DAYS_BEFORE=1
 IMG_PNG_1024_PATH=/imgsrv/png/new/1024
 IMG_PNG_1500_PATH=/imgsrv/png/new/1500
+USE_DB=1
 
 # Script
 PYTHON3=/usr/bin/python3
 FIND=/usr/bin/find
-DCM_RETRIEVER=/app/retrieve_cxr_by_datehour_dcmtk.py
+DCM_RETRIEVER=/app/retrieve_cxr_not_processed_dcmtk.py
 CAFFE_FORWARDER=/app/convert_then_forward.py
 KERAS_PREDICTER=/app/predict_share_img.py
 PNG_CONVERTER=/app/convert_png_mp.py
@@ -21,14 +23,13 @@ cd $WORKDIR
 echo "Started at: $(TZ="Asia/Taipei" date)"
 
 # Retrieve images
-DATEHOUR=$(TZ="Asia/Taipei" date -d '1 hour ago' "+%Y-%m-%d %H")
-$PYTHON3 $DCM_RETRIEVER $DATEHOUR $DCM_TMPDIR
+$PYTHON3 $DCM_RETRIEVER $DCM_TMPDIR $DAYS_BEFORE
 
 # Convert 1024 png and then Caffe model forwarding
-$PYTHON3 $CAFFE_FORWARDER $DCM_TMPDIR $IMG_PNG_1024_PATH 1
+$PYTHON3 $CAFFE_FORWARDER $DCM_TMPDIR $IMG_PNG_1024_PATH $USE_DB
 
 # Keras model predicting
-$PYTHON3 $KERAS_PREDICTER $DCM_TMPDIR 1
+$PYTHON3 $KERAS_PREDICTER $DCM_TMPDIR $USE_DB
 
 # Convert images
 ## Only need to convert 1500 png. 1024 was done with Caffe forwarding
